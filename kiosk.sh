654a4425097539a54cc877e4deb37959af36cbfe
Kelvin Romero <kelvinromero@gmail.com>
2017-08-23 19:11:42 +0000

    corrections

--- kiosk.sh
+++ kiosk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Avoids crashed/recovered sessions
-killall chromium-browser
+# killall chromium-browser
 
 #Check for URL updates
 git pull
-url=`cat address`
+url=$(cat address)
 
 #Starts kioks
 `/usr/bin/chromium-browser --noerrdialogs --disable-session-crashed-bubble --disable-infobars --kiosk $url`
