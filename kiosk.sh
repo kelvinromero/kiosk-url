c13541042fa4a02a1bd90505f32dfe7c7ef56780
Kelvin Romero <kelvin.romero@gmail.com>
2017-08-24 13:09:49 -0300

    Updating chromium cleaning to version 56

--- kiosk.sh
+++ kiosk.sh
@@ -8,9 +8,11 @@ git pull
 url=$(cat /home/pi/kiosk-url/address)
 
 #Stops chromium crash messages
-sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/chromium/Default/Preferences
-sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/chromium/Default/Preferences
+# sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/chromium/Default/Preferences
+# sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/chromium/Default/Preferences
+
+sed -i -e 's/("exit_type":\s*)"Crashed"/\1"None"/g' \ -e 's/("exited_cleanly":\s*)false/\1true/g' \ ~/.config/chromium/Default/Preferences
 
 
 #Starts kioks
-`/usr/bin/chromium-browser --kiosk --no-default-browser-check --no-first-run --disable-infobars --disable-session-crashed-bubble $url`
+`/usr/bin/chromium-browser --kiosk --no-default-browser-check --no-first-run --disable-infobars --disable-session-crashed-bubble   $url`
