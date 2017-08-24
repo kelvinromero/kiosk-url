b1dc75cd9cffc2eeb6675b772b49545ee0586f46
Kelvin Romero <kelvin.romero@gmail.com>
2017-08-24 12:46:59 -0300

    Icognito didn't work, trying another flag.

--- kiosk.sh
+++ kiosk.sh
@@ -7,5 +7,10 @@
 git pull
 url=$(cat /home/pi/kiosk-url/address)
 
+#Stops chromium crash messages
+# sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/google-chrome/Default/Preferences
+# sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/google-chrome/Default/Preferences
+
+
 #Starts kioks
-`/usr/bin/chromium-browser --icognito --kiosk $url`
+`/usr/bin/chromium-browser --kiosk --icognito --disable-session-crashed-bubble $url`
