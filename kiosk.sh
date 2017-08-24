da17696a6bc40dd17ffb75eaafd2abe83e47a77f
Kelvin Romero <kelvin.romero@gmail.com>
2017-08-24 12:53:39 -0300

    Cleaning error messages.

--- kiosk.sh
+++ kiosk.sh
@@ -8,9 +8,9 @@ git pull
 url=$(cat /home/pi/kiosk-url/address)
 
 #Stops chromium crash messages
-# sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/google-chrome/Default/Preferences
-# sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/google-chrome/Default/Preferences
+sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/chromium/Default/Preferences
 
 
 #Starts kioks
-`/usr/bin/chromium-browser --kiosk --icognito --disable-session-crashed-bubble $url`
+`/usr/bin/chromium-browser --kiosk --no-default-browser-check --no-first-run --disable-infobars --disable-session-crashed-bubble $url`
