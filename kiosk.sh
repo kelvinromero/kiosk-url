cf80ad35b3d3834c2ce5356e170ed522e4a1849b
Kelvin Romero <kelvin.romero@gmail.com>
2017-08-24 13:15:19 -0300

    Local state shit

--- kiosk.sh
+++ kiosk.sh
@@ -8,11 +8,8 @@ git pull
 url=$(cat /home/pi/kiosk-url/address)
 
 #Stops chromium crash messages
-# sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/chromium/Default/Preferences
-# sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/chromium/Default/Preferences
-
-sed -i -e 's/("exit_type":\s*)"Crashed"/\1"None"/g' \ -e 's/("exited_cleanly":\s*)false/\1true/g' \ ~/.config/chromium/Default/Preferences
-
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
 
 #Starts kioks
 `/usr/bin/chromium-browser --kiosk --no-default-browser-check --no-first-run --disable-infobars --disable-session-crashed-bubble   $url`
