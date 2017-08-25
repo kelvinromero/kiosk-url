a6cb791d29c44700a570cfd883be699e77711c0e
Kelvin Romero <kelvin.romero@gmail.com>
2017-08-25 14:14:53 -0300

    Rotate screen

--- config.sh
+++ config.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 # Configures the system to start the kiosk right after the gnome.
 
+# Only root can run the script
+if [[ $EUID -ne 0 ]]; then
+   printf "Execute esse Script como root:\n$ sudo su\n\n" 1>&2
+   exit 1
+fi
 
 # Desktop file
-file="~/.config/autostart/autostartkiosk.desktop"
+file="/home/pi/.config/autostart/autostartkiosk.desktop"
 
 if [[ ! -f $file ]]; then
-    cp autostartkiosk.desktop ~/.config/autostart/
+    cp autostartkiosk.desktop /home/pi/.config/autostart/
+    chown pi:pi /home/pi/.config/autostart/autostartkiosk.desktop
+fi
+
+# Rotate display
+rotated=$(cat /boot/config.txt | grep display_rotate=3)
+if [ -z "$rotated" ]; then
+  echo "display_rotate=3" >> /boot/config.txt
 fi
