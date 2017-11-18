3b7eea19dd7ecd8028bd7b1842eb2138092e9694
Kelvin Romero <kelvin.romero@gmail.com>
2017-11-18 14:43:52 -0300

    Comments on what to update

--- kiosk.sh
+++ kiosk.sh
@@ -3,10 +3,18 @@
 # Avoid crashed/recovered sessions
 killall chromium-browser
 
+wget -q --tries=10 --timeout=20 --spider http://github.com
+
+# Check internet connection
+# if sucessful pull data from github
 #Check for (URL and code) updates
 git pull
-url=$(cat /home/pi/kiosk-url/address)
 
+# if not pull from admin at local network
+get local_net_ip:80/address > /home/pi/kiosk-url/address
+
+# Finally
+url=$(cat /home/pi/kiosk-url/address)
 #Stop chromium crash messages
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
